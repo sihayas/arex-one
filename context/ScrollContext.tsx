c05ea24e69eaffe1c956aced7b9e02079263871e
coratype <coratype@icloud.com>
2023-08-09 16:09:24 -0400

    ScrollContext type fix

--- context/ScrollContext.tsx
+++ context/ScrollContext.tsx
@@ -1,16 +1,16 @@
-import React, { useContext, Dispatch, SetStateAction } from "react";
+import React, { useContext, Dispatch, SetStateAction, ReactNode } from "react";
 
-interface IScrollContext {
+interface ScrollContext {
   cursorOnRight: boolean;
   setCursorOnRight: Dispatch<SetStateAction<boolean>>;
 }
 
-const ScrollContext = React.createContext<IScrollContext>({
+const ScrollContext = React.createContext<ScrollContext>({
   cursorOnRight: false,
   setCursorOnRight: () => {}, // Default function does nothing
 });
 
-export const ScrollProvider = ({ children }) => {
+export const ScrollProvider = ({ children }: { children: ReactNode }) => {
   const [cursorOnRight, setCursorOnRight] = React.useState(false);
 
   return (
