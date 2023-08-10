2b2006f3eaa7dbff8201c7fa81bc2f9c0a88aa17
coratype <coratype@icloud.com>
2023-08-09 22:02:48 -0400

    disabled open ai cron

--- pages/api/cron/generateConsensus.ts
+++ pages/api/cron/generateConsensus.ts
@@ -1,18 +1,18 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { generateConsensusForAlbums } from "@/lib/global/openAI";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { generateConsensusForAlbums } from "@/lib/global/openAI";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST" || req.method === "GET") {
-    try {
-      await generateConsensusForAlbums();
-      res.status(200).json({ message: "Successfully updated consensus'" });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Failed to update consensus'." });
-    }
-  } else {
-    res.status(405).json({ message: "Method not allowed." });
-  }
-};
+// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method === "POST" || req.method === "GET") {
+//     try {
+//       await generateConsensusForAlbums();
+//       res.status(200).json({ message: "Successfully updated consensus'" });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ message: "Failed to update consensus'." });
+//     }
+//   } else {
+//     res.status(405).json({ message: "Method not allowed." });
+//   }
+// };
 
-export default handler;
+// export default handler;
