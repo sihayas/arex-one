027d1a8a821066a6f6e1baf39d7e95cf75a0b99d
coratype <coratype@icloud.com>
2023-08-09 22:01:40 -0400

    modified next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,28 +1,54 @@
-import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
-import { prisma } from "../../../lib/global/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
-export default authHandler;
+const prisma = new PrismaClient();
 
-//Initialize connection to Google Database
-const options = {
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
   callbacks: {
     async session({ session, user }) {
       session.user.id = user.id;
-      session.user.name = user.name;
-      session.user.gender = user.profile ? user.profile.gender : null;
+      // session.user.name = user.name;
       return session;
     },
   },
-};
+});
+
+// import NextAuth from "next-auth";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { prisma } from "../../../lib/global/prisma";
+// import GoogleProvider from "next-auth/providers/google";
+// import { NextApiHandler } from "next";
+
+// const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+// export default authHandler;
+
+// const prismaAdapter = PrismaAdapter(prisma);
+
+// //Initialize connection to Google Database
+// const options = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   adapter: prismaAdapter,
+//   secret: process.env.SECRET,
+//   callbacks: {
+//     async session({ session, user }) {
+//       session.user.id = user.id;
+//       session.user.name = user.name;
+//       return session;
+//     },
+//   },
+// };
