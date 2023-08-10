99e2190f712c63c68dfb9aa65b48a91af22dfb6a
coratype <coratype@icloud.com>
2023-08-09 22:06:17 -0400

    cross ref search type fix

--- pages/api/crossReferenceSearch.ts
+++ pages/api/crossReferenceSearch.ts
@@ -1,12 +1,24 @@
 // Cross reference Apple results with rating in cache, utilized in SearchAlbums.tsx
+import { NextApiRequest, NextApiResponse } from "next";
 import { searchAlbums } from "../../lib/global/musicKit";
 import client from "../../lib/global/redis";
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { query } = req.query;
 
+  if (typeof query === "undefined") {
+    res.status(400).json({ error: "Query parameter is required" });
+    return;
+  }
+
+  // Convert query to string if it's an array
+  const keyword = typeof query === "string" ? query : query.join(" ");
+
   // Fetch albums from Apple's database
-  const appleAlbums = await searchAlbums(query);
+  const appleAlbums = await searchAlbums(keyword);
 
   // Cross-reference the Apple albums with our own database
   for (const album of appleAlbums) {
