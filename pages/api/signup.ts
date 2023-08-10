b4a062e60326537cfe8167ee24601b0a530d4a8f
coratype <coratype@icloud.com>
2023-08-09 22:15:39 -0400

    signup type error

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,8 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "POST") {
     const { email, password, username } = req.body;
 
