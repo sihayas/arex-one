eccb694d71cbb83e8ba38f0a13973dee10107696
coratype <coratype@icloud.com>
2023-08-12 00:42:09 -0400

    user page final?

--- components/cmdk/pages/user/subcomponents/Soundtrack.tsx
+++ components/cmdk/pages/user/subcomponents/Soundtrack.tsx
@@ -9,9 +9,12 @@ type SoundtrackProps = {
 // EntrySection Component
 const Soundtrack: React.FC<SoundtrackProps> = ({ reviews }) => {
   return (
-    <div className="flex flex-col mt-[80px] pt-7 gap-4 w-full items-center">
+    <div className="flex flex-col mt-[80px] pt-7 gap-8 w-full items-center">
       {reviews.map((review: ReviewData, i: number) => (
-        <EntryUser key={i.toString()} review={review} />
+        <>
+          <EntryUser key={i.toString()} review={review} />
+          <div className="w-[90%] h-[1px] bg-silver"></div>
+        </>
       ))}
     </div>
   );
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,10 @@
 import { CMDK } from "./cmdk/CmdK";
-import React, { useEffect, ReactNode, useCallback } from "react";
+import React, { useEffect, ReactNode, useCallback, useRef } from "react";
 import { useCMDK } from "@/context/CMDKContext";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const { isVisible, setIsVisible } = useCMDK();
+  const mainContentRef = useRef<HTMLElement>(null);
 
   const handleKeyDown = useCallback((event: KeyboardEvent) => {
     if ((event.metaKey || event.ctrlKey) && event.key === "k") {
@@ -21,11 +22,29 @@ export default function Layout({ children }: { children: ReactNode }) {
     };
   }, [handleKeyDown]);
 
+  const handleDoubleClick = useCallback(() => {
+    setIsVisible((prevIsVisible) => !prevIsVisible);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    const mainContentElement = mainContentRef.current;
+    if (mainContentElement) {
+      mainContentElement.addEventListener("dblclick", handleDoubleClick);
+    }
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      if (mainContentElement) {
+        mainContentElement.removeEventListener("dblclick", handleDoubleClick);
+      }
+    };
+  }, [handleKeyDown, handleDoubleClick]);
+
   return (
     <>
       <CMDK isVisible={isVisible} />
-      {/* <AnimatedGradient /> */}
       <main
+        ref={mainContentRef}
         id="main-content"
         className={`h-screen grid relative ${
           isVisible ? "animate-scale-down" : "animate-scale-up"
@@ -36,5 +55,3 @@ export default function Layout({ children }: { children: ReactNode }) {
     </>
   );
 }
-
-// Un-render the CMDK component when it's not visible
