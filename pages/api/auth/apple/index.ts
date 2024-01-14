ea983eb26d4988f6dd97797690526a5e5b75d575
coratype <coratype@icloud.com>
2024-01-14 02:18:54 -0500

    Lucia Auth

--- pages/api/auth/apple/index.ts
+++ pages/api/auth/apple/index.ts
@@ -8,23 +8,37 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== "GET") {
-    res.status(404).end();
-    return;
+  try {
+    if (req.method !== "GET") {
+      // Log and return a 405 Method Not Allowed error if not a GET request
+      console.error("Invalid request method:", req.method);
+      res.status(405).end("Method Not Allowed");
+      return;
+    }
+
+    const state = generateState();
+    const url: URL = await apple.createAuthorizationURL(state, {
+      scopes: ["name", "email"],
+    });
+    url.searchParams.set("response_mode", "query");
+
+    res
+      .appendHeader(
+        "Set-Cookie",
+        serializeCookie("apple_oauth_state", state, {
+          path: "/",
+          secure: process.env.NODE_ENV === "production",
+          httpOnly: true,
+          maxAge: 60 * 10,
+          sameSite: "lax",
+        }),
+      )
+      .redirect(url.toString());
+  } catch (error) {
+    // Log the error for debugging
+    console.error("Error in /api/auth/apple handler:", error);
+
+    // Send a generic 500 Internal Server Error response
+    res.status(500).end("Internal Server Error");
   }
-  const state = generateState();
-  const url = await apple.createAuthorizationURL(state);
-  url.searchParams.set("response_mode", "query");
-  res
-    .appendHeader(
-      "Set-Cookie",
-      serializeCookie("apple_oauth_state", state, {
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-        httpOnly: true,
-        maxAge: 60 * 10,
-        sameSite: "lax",
-      }),
-    )
-    .redirect(url.toString());
 }
