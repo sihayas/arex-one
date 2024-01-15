9a55580219b11cbe5dc4784074b28b172d084894
coratype <coratype@icloud.com>
2024-01-15 02:11:13 -0500

    Lucia Auth

--- pages/api/auth/apple/callback.ts
+++ pages/api/auth/apple/callback.ts
@@ -10,8 +10,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== "GET") {
-    res.status(404).end();
+  if (req.method !== "POST") {
+    res.status(405).end("Method Not Allowed");
     return;
   }
 
