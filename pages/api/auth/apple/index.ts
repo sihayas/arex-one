0ef29141c2ddf2e0f8e50dceb8eab687297b2dba
coratype <coratype@icloud.com>
2024-01-15 00:57:07 -0500

    Lucia Auth

--- pages/api/auth/apple/index.ts
+++ pages/api/auth/apple/index.ts
@@ -22,6 +22,8 @@ export default async function handler(
     });
     url.searchParams.set("response_mode", "form_post");
 
+    console.log("State:", state);
+
     res
       .appendHeader(
         "Set-Cookie",
