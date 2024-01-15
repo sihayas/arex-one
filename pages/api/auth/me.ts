02711eea82ccf60d55ec0e27b53d60b88b763455
coratype <coratype@icloud.com>
2024-01-15 01:37:16 -0500

    Lucia Auth

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,20 +1,20 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { lucia } from "@/lib/global/auth";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  const sessionId = lucia.readSessionCookie(req.headers.cookie ?? "");
-  if (!sessionId) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  const { user, session } = await lucia.validateSession(sessionId);
-  if (!session) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  // Return the user's data and session info
-  res.json({ user, session });
-}
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { lucia } from "@/lib/global/auth";
+//
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+// ) {
+//   const sessionId = lucia.readSessionCookie(req.headers.cookie ?? "");
+//   if (!sessionId) {
+//     return res.status(401).json({ error: "Unauthorized" });
+//   }
+//
+//   const { user, session } = await lucia.validateSession(sessionId);
+//   if (!session) {
+//     return res.status(401).json({ error: "Unauthorized" });
+//   }
+//
+//   // Return the user's data and session info
+//   res.json({ user, session });
+// }
