821b0d3d25350c29b9047a4f1e84a173d32acb75
coratype <coratype@icloud.com>
2024-01-27 05:01:56 -0500

    changed entry

--- components/global/EntryDial.tsx
+++ components/global/EntryDial.tsx
@@ -55,7 +55,7 @@ const calculateStrokeLength = (
 
 const EntryDial = ({ rating }: EntryDialProps) => {
   const dialRef = useRef<SVGSVGElement>(null);
-  const strokeWidth = 1.5;
+  const strokeWidth = 2.5;
   const dotRadius = 1;
 
   const totalSize = 32;
--- components/index/Entry.tsx
+++ components/index/Entry.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import useHandleHeartClick from "@/hooks/useHeart";
 import { useArtifact } from "@/hooks/usePage";
@@ -6,18 +6,36 @@ import { useArtifact } from "@/hooks/usePage";
 import Avatar from "@/components/global/Avatar";
 import Heart from "@/components/global/Heart";
 import { ArtifactExtended } from "@/types/globalTypes";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useAnimation,
+  useMotionValueEvent,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 import Image from "next/image";
 import EntryDial from "@/components/global/EntryDial";
 import { useInterfaceContext } from "@/context/InterfaceContext";
-import { MaskCardBottom, MaskCardTop } from "@/components/icons";
 
 interface NewAProps {
   artifact: ArtifactExtended;
 }
 
+const maskStyle = {
+  maskImage: "url('/images/mask_card_top_outlined.svg')",
+  maskSize: "cover",
+  maskRepeat: "no-repeat",
+  WebkitMaskImage: "url('/images/mask_card_top_outlined.svg')",
+  WebkitMaskSize: "cover",
+  WebkitMaskRepeat: "no-repeat",
+};
+
 export const Entry: React.FC<NewAProps> = ({ artifact }) => {
   const { user } = useInterfaceContext();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const controls = useAnimation();
+  const [rotate, setRotate] = React.useState(false);
 
   const sound = artifact.appleData;
   const artwork = MusicKit.formatArtworkURL(sound.attributes.artwork, 540, 540);
@@ -26,6 +44,21 @@ export const Entry: React.FC<NewAProps> = ({ artifact }) => {
   const apiUrl = artifact.heartedByUser
     ? "/api/heart/delete/artifact"
     : "/api/heart/post/artifact";
+
+  const { scrollY } = useScroll({
+    container: containerRef,
+  });
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    console.log(latest);
+  });
+
+  // const scale = useSpring(useTransform(scrollY, [432, 400], [1, 1.1]), {
+  //   stiffness: 300,
+  //   damping: 30,
+  // });
+
+  // Flip as we scroll
 
   const { hearted, handleHeartClick, heartCount } = useHandleHeartClick(
     artifact.heartedByUser,
@@ -37,19 +70,10 @@ export const Entry: React.FC<NewAProps> = ({ artifact }) => {
     user?.id,
   );
 
-  const handleEntryClick = useArtifact(artifact);
-
-  const maskStyle = {
-    maskImage: "url('/images/mask_card_top.svg')",
-    maskSize: "cover",
-    maskRepeat: "no-repeat",
-    WebkitMaskImage: "url('/images/mask_card_top.svg')",
-    WebkitMaskSize: "cover",
-    WebkitMaskRepeat: "no-repeat",
-  };
-
   return (
-    <div className={`flex items-end gap-2.5 relative group w-[356px] group`}>
+    <div
+      className={`flex items-end gap-2.5 relative group w-[356px] group over`}
+    >
       <Avatar
         className={`h-[42px] border border-silver z-10`}
         imageSrc={artifact.author.image}
@@ -65,40 +89,47 @@ export const Entry: React.FC<NewAProps> = ({ artifact }) => {
           width: 304,
           height: 400,
           ...maskStyle,
+          // scale,
+          transformStyle: "preserve-3d",
         }}
-        className={`flex flex-col will-change-transform bg-white relative z-10 overflow-scroll`}
+        ref={containerRef}
+        className={`flex flex-col will-change-transform bg-white relative z-10 scrollbar-none overflow-y-auto snap-y snap-mandatory overscroll-contain`}
       >
-        <Image
-          className={`outline outline-1 outline-silver min-w-[304px] min-h-[304px] -mt-8`}
-          src={artwork}
-          alt={`artwork`}
-          loading="lazy"
-          quality={100}
-          width={304}
-          height={304}
-        />
-        {/* Content Container */}
-        <div className={`px-6 pt-4 pb-2 flex items-center gap-2`}>
-          <EntryDial rating={artifact.content!.rating!} />
+        <div className={`min-w-full min-h-full bg-red/50`}></div>
+        <motion.div
+          animate={controls}
+          className={`flex flex-col -scroll-mt-8 snap-start`}
+        >
+          <Image
+            className={`outline outline-1 outline-silver min-w-[304px] min-h-[304px]`}
+            src={artwork}
+            alt={`artwork`}
+            loading="lazy"
+            quality={100}
+            width={304}
+            height={304}
+          />
+          {/* Content Container */}
+          <div className={`px-6 pt-4 pb-2 flex items-center gap-2`}>
+            <EntryDial rating={artifact.content!.rating!} />
 
-          <div className={`flex flex-col`}>
-            <div className={`text-sm text-black line-clamp-1`}>
-              {sound.attributes.artistName}
-            </div>
-            <div className={`font-semibold text-base text-black line-clamp-1`}>
-              {sound.attributes.name}
+            <div className={`flex flex-col`}>
+              <div className={`text-sm text-gray2 line-clamp-1`}>
+                {sound.attributes.artistName}
+              </div>
+              <div
+                className={`font-semibold text-base text-black line-clamp-1`}
+              >
+                {sound.attributes.name}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className={`text-base text-gray4 px-6`}>
-          {artifact.content?.text}
-        </div>
+          <div className={`text-base text-black px-6`}>
+            {artifact.content?.text}
+          </div>
+        </motion.div>
       </motion.div>
-
-      <div className={`absolute bottom-0 right-0 cloud-shadow`}>
-        <MaskCardTop />
-      </div>
 
       {/* Ambien */}
       <motion.div
@@ -121,15 +152,3 @@ export const Entry: React.FC<NewAProps> = ({ artifact }) => {
     </div>
   );
 };
-
-// const handleSoundClick = async () => {
-//   playContent(sound.id, sound.type);
-// };
-
-// const handleSoundClick = async () => {
-//     handleSelectSound(sound);
-// };
-//
-
-// const { playContent } = useSoundContext();
-//
