ee4dac5e08c03f854fd8a56d90ec359eb573a592
coratype <coratype@icloud.com>
2024-01-27 07:10:58 -0500

    better entry

--- components/index/Entry.tsx
+++ components/index/Entry.tsx
@@ -11,12 +11,23 @@ import {
   useAnimation,
   useMotionValueEvent,
   useScroll,
-  useSpring,
-  useTransform,
 } from "framer-motion";
 import Image from "next/image";
 import EntryDial from "@/components/global/EntryDial";
 import { useInterfaceContext } from "@/context/InterfaceContext";
+import {
+  FiveStar,
+  FourHalfStar,
+  FourStar,
+  HalfStar,
+  MaskCardTopOutlined,
+  OneHalfStar,
+  OneStar,
+  ThreeHalfStar,
+  ThreeStar,
+  TwoHalfStar,
+  TwoStar,
+} from "@/components/icons";
 
 interface NewAProps {
   artifact: ArtifactExtended;
@@ -29,6 +40,24 @@ const maskStyle = {
   WebkitMaskImage: "url('/images/mask_card_top_outlined.svg')",
   WebkitMaskSize: "cover",
   WebkitMaskRepeat: "no-repeat",
+};
+
+const starComponents = {
+  0.5: HalfStar,
+  1: OneStar,
+  1.5: OneHalfStar,
+  2: TwoStar,
+  2.5: TwoHalfStar,
+  3: ThreeStar,
+  3.5: ThreeHalfStar,
+  4: FourStar,
+  4.5: FourHalfStar,
+  5: FiveStar,
+};
+
+const getStarComponent = (rating: number) => {
+  const StarComponent = starComponents[Math.ceil(rating * 2) / 2];
+  return StarComponent ? <StarComponent /> : null;
 };
 
 export const Entry: React.FC<NewAProps> = ({ artifact }) => {
@@ -87,49 +116,53 @@ export const Entry: React.FC<NewAProps> = ({ artifact }) => {
       <motion.div
         style={{
           width: 304,
-          height: 400,
+          height: 432,
           ...maskStyle,
-          // scale,
-          transformStyle: "preserve-3d",
         }}
         ref={containerRef}
-        className={`flex flex-col will-change-transform bg-white relative z-10 scrollbar-none overflow-y-auto snap-y snap-mandatory overscroll-contain`}
+        className={`flex flex-col will-change-transform bg-white relative z-10`}
       >
-        <div className={`min-w-full min-h-full bg-red/50`}></div>
-        <motion.div
-          animate={controls}
-          className={`flex flex-col -scroll-mt-8 snap-start`}
+        {/* Content Container */}
+        <div
+          className={`absolute bottom-0 left-0 p-4 flex items-center gap-2 z-50`}
         >
-          <Image
-            className={`outline outline-1 outline-silver min-w-[304px] min-h-[304px]`}
-            src={artwork}
-            alt={`artwork`}
-            loading="lazy"
-            quality={100}
-            width={304}
-            height={304}
-          />
-          {/* Content Container */}
-          <div className={`px-6 pt-4 pb-2 flex items-center gap-2`}>
-            <EntryDial rating={artifact.content!.rating!} />
+          {getStarComponent(artifact.content!.rating)}
 
-            <div className={`flex flex-col`}>
-              <div className={`text-sm text-gray2 line-clamp-1`}>
-                {sound.attributes.artistName}
-              </div>
-              <div
-                className={`font-semibold text-base text-black line-clamp-1`}
-              >
-                {sound.attributes.name}
-              </div>
-            </div>
+          <div className={`font-semibold text-base text-black line-clamp-1`}>
+            {sound.attributes.name}
           </div>
+        </div>
+        <Image
+          src={artwork}
+          alt={`artwork`}
+          loading="lazy"
+          quality={100}
+          width={304}
+          height={304}
+        />
 
-          <div className={`text-base text-black px-6`}>
-            {artifact.content?.text}
-          </div>
-        </motion.div>
+        <div className={`text-base text-black px-6 pt-2.5 `}>
+          {artifact.content?.text}
+        </div>
+        <motion.div
+          style={{
+            background: `linear-gradient(180deg, rgba(255, 255, 255, 0.00) 0%, #FFF 24%, #FFF 55.32%)`,
+            height: "3rem",
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+            zIndex: 10,
+          }}
+          className={`absolute left-0 w-full`}
+        />
       </motion.div>
+
+      <div
+        className={`absolute bottom-0 right-0 w-[304px] h-[432px] cloud-shadow`}
+      >
+        <MaskCardTopOutlined />
+      </div>
 
       {/* Ambien */}
       <motion.div
