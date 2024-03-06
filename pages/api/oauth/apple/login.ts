b04694bd872a8f053c46423e7773e16c8c72e19b
coratype <coratype@icloud.com>
2024-03-06 18:29:45 -0500

    removed thing

--- pages/api/oauth/apple/login.ts
+++ pages/api/oauth/apple/login.ts
@@ -10,7 +10,6 @@ export default async function handler(
 ) {
   try {
     if (req.method !== "GET") {
-      // Log and return a 405 Method Not Allowed error if not a GET request
       console.error("Invalid request method:", req.method);
       res.status(405).end("Method Not Allowed");
       return;
@@ -22,34 +21,32 @@ export default async function handler(
     });
     url.searchParams.set("response_mode", "form_post");
 
-    // Generate the cookie string
     const cookieString = serializeCookie("apple_oauth_state", state, {
       path: "/",
-      secure: true, // Important, treat development as secure for Apple
+      secure: true,
       httpOnly: true,
       maxAge: 60 * 10, // 10 minutes
       sameSite: "none",
     });
 
-    // Attach the cookie to the response
-    res.setHeader("Set-Cookie", cookieString); // Note: It's setHeader, not appendHeader. Verify this based on your framework version
+    res.setHeader("Set-Cookie", cookieString);
     res.redirect(url.toString());
-  } catch (error) {
-    // Enhanced error logging
-    console.error("Error in /api/oauth/apple handler:", {
-      message: error.message,
-      stack: error.stack,
-      // Include any other relevant info about the error
-      // You can customize this part to log more error details specific to your application or the external services it interacts with
-    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error in /api/oauth/apple handler:", {
+        message: error.message,
+        stack: error.stack,
+      });
 
-    // Optionally, send more detailed error information back to the client in development mode
-    // Ensure to not expose sensitive information in production
-    if (process.env.NODE_ENV === "development") {
-      res
-        .status(500)
-        .json({ error: "Internal Server Error", details: error.message });
+      if (process.env.NODE_ENV === "development") {
+        res
+          .status(500)
+          .json({ error: "Internal Server Error", details: error.message });
+      } else {
+        res.status(500).end("Internal Server Error");
+      }
     } else {
+      console.error("An unexpected error occurred", error);
       res.status(500).end("Internal Server Error");
     }
   }
